786b2e1ac7c7e98abfb13d9367dbb9d694ff0afd
Chameleon <magic.alone.k@gmail.com>
2018-05-06 18:32:33 +0800

    master

---
+++ at-cat-babylon/src/main.js
@@ -0,0 +1,177 @@
+/** Created by chameleon <magic.alone.k@gmail.com>
+ * ╭────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────╮
+ * │                                                                                                                                                        │
+ * │                                      ;XBX3                                                                                                             │
+ * │                                     X@3;5@@r                                                       ;995                                                │
+ * │                                     @@    9@        :riSS9S@@@@@@Bi                              9@@G9B@s                                              │
+ * │                             B@@@B;   @B    @A:5GA9BX93S5s,@@:   ;iB@@                           @@     @@                                              │
+ * │                            @@  :9@@   @B:sB@BG3;rB9AA;    @r     ,r B@,            ;r35G9AGAGA5X@X :   @A     5@@@@S                                   │
+ * │                            @@     B@,;B@@rr;    @rs: @@   B@  @@ B@  B@      :5X@@@BBXAS533s5rrX@r9B@@@@Br  X@A;: r@@                                  │
+ * │                             @@;   r@@X:  A59@B ;@9@B:@B    @@        5@    B@@@BSs;           ,S5;    S5;B@@@r      @3                                 │
+ * │                              r@BB@B:   ;@;3: @@ ;BXBB;      B@9:    A@9  B@B r;    S3        @@;ri@B        i@@r  X@B                                  │
+ * │                               ,@@s     s@5@B @B               SBAB@@@;  @@   B;    rB       :@5 @XX@ :@@XBX   s@@@B:                                   │
+ * │                              9@,        ;XAABs                  9@@r   @@     95sr55:        :B@BB@  @9 rr3@   r@X                                     │
+ * │                             9@                               9@@@;     @5       ::                   @B:@B3@    ,@                                     │
+ * │                            :@:  i33s53r                  5@@@Xr       ;@s                             9A59B      @9                                    │
+ * │                            @@  B;    :rB;                ,:@@          @@                              ,r;,;i5;  @B                                    │
+ * │                            @X rG        @                   @B         S@: 9@                        :B;      Si @X                                    │
+ * │                            @X  @        B               @;  ;@          B@  @B          AA           @:        G @;                                    │
+ * │                            @@   93;  :rB;  5r          @@    @r          A@r 5@BSr;;r5@@@r           X3       9 @@                                     │
+ * │                            ;@s   ,rsr3r    A@@Ar;,,rS@@X    5@            :@@;  rABBBBS:              95;;;rsi B@                                      │
+ * │                             B@;              rB@@@@BG;     ;@5              r@@A:                       ;;;   @@:                                      │
+ * │                              X@9                          G@i                  9@@@S,                       A@S                                        │
+ * │                               ,@@5                     r@@@           :9r          @@@@@@BX3r;,       ;rSB@@@@;                                        │
+ * │                                X@@@@Ar:           ,3X@@@sr@@        ,@@@@@A5GXAA5r9@s   ;39BB@@@@@@@@@@@BS,  r@@                                       │
+ * │                               @@   rB@@@@@@@@@@@@@@@9;     @@@@@@@@@@@@@@@@B@BB9G@@:                           @@                                      │
+ * │                              @@          ,;;;;,:            B@:      sX ;       G@  :                           @@                                     │
+ * │                             @@                              :@@       ;BX;  ;i3B@@@@B                            @@                                    │
+ * │                            5@r                             9B@@@@@@@@@@@@@@@@@@@X                                r@r                                   │
+ * │                            @@                                 3@r    ,@@@@B   @@                                  @@                                   │
+ * │                      rBX: 9@                                   @@      :s:   ,@A                                  ,@GABBBs                             │
+ * │                  S@:r@B@@ @@                                   5@;           @@                                    @@; :@@B;                           │
+ * │                  :@@@@@@@9@r                                    @@           @@                                    S@   :3;@3                          │
+ * │                     ,: :;@@                                     @@          i@i                                     @B   B@@:                          │
+ * │                          @B                                     r@;         @@                                      @@@BB@G                            │
+ * │                         s@9                                     :@@         @@;;rrr;;;;::   ::;;;;;;r;;;;,: ;;ri3s3;@@, :                              │
+ * │                         r@B@@@BB9X@@B9XB@@@@@@@@BGSX@@9GB@B@B@@@B@9         r3SXAXAXGG9X@@@BAGXBBBBBXBGA9A@@@X5S99993s                                 │
+ * │                                 BX@@@@;          9@@@@@B                               @BB@9@5          B@B@BB@                                        │
+ * │                                9@    @@,;,       @B   5@r;;                        S9S@@    @@      r9SS@B   :@;                                       │
+ * │                                9@    ;5r3X@;     @5    33r5@@                     @9        @@     B@         @:                                       │
+ * │                                A@:       ,@5     @B        @@                     @Brr;rs33s@@     X@i;r;is3iB@:                                       │
+ * │                                 9599A9GSS5;      r95AGA9GSSr                        ;rrrirr;,        ;rririrr;                                         │
+ * │                                                                                                                                                        │
+ * ├────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┤
+ * │                                                          ____ ____ ___     ___  _    ____ ____ ____      _  _ ____ _  _ ____ ____    ___  _  _ ____    │
+ * │                                                          | __ |  | |  \    |__| |    |___ |__  |__       |\ | |___ |  | |___ |__/    |__| |  | | __    │
+ * │                                                          |__| |__| |__/    |__| |___ |___ ___| ___| ,    | \| |___  \/  |___ |  \    |__| |__| |__|    │
+ * │                                                                                                                                                        │
+ * ├────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┤
+ * │                                                                                                                 __ .  ..__..  ..___.   .___.__..  .    │
+ * │                                                                                                                /  `|__||__||\/||__ |   |__ |  ||\ |    │
+ * │                                                                                                                \__.|  ||  ||  ||___|___|___|__|| \|    │
+ * │                                                                                                                                                        │
+ * ├────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┤
+ * │ Description: Program entry file.                                                                                                                       │
+ * │ Author: Chameleon <magic.alone.k@gmail.com>                                                                                                            │
+ * │ Version: 1.0.0.0                                                                                                                                       │
+ * │ Company: HeFei Parity Ltd.                                                                                                                             │
+ * ╰────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────╯
+ */
+
+import BABYLON from 'babylonjs'
+import 'babylonjs-materials'
+import 'babylonjs-loaders'
+import './lib/FurMaterial'
+
+// import SceneLoading from './scenes/SceneLoading'
+import Scenehouse from './scenes/SceneHouse'
+
+const SHADOW_GENERATOR_SIZE = 512
+const DEBUG = true
+
+class Game {
+  /** Canvas DOM 元素
+   * @type {HTMLCanvasElement}
+   */
+  get canvas () { return this._canvas }
+  set canvas (val) { this._canvas = val }
+  /** Babylon 引擎
+   * @type {BABYLON.Engine}
+   */
+  get engine () { return this._engine }
+  set engine (val) { this._engine = val }
+  /** 主场景
+   * @type {BABYLON.Scene}
+   */
+  get scene () { return this._scene }
+  set scene (val) { this._scene = val }
+  /** 主摄像机
+   * @type {BABYLON.Camera}
+   */
+  get camera () { return this._camera }
+  set camera (val) { this._camera = val }
+  /** 天空盒
+   * @type {BABYLON.Mesh}
+   */
+  get skybox () { return this._skybox }
+  set skybox (val) { this._skybox = val }
+  /** 主光源
+   * @type {BABYLON.Light}
+   */
+  get light () { return this._light }
+  set light (val) { this._light = val }
+  /** 主阴影
+   * @type {BABYLON.ShadowGenerator}
+   */
+  get shadowGenerator () { return this._shadowGenerator }
+  set shadowGenerator (val) { this._shadowGenerator = val }
+  /** 默认渲染管线
+   * @type {BABYLON.PostProcessRenderPipeline}
+   */
+  get pipeline () { return this._pipeline }
+  set pipeline (val) { this._pipeline = val }
+  /** 资源管理器
+   * @type {BABYLON.AssetsManager}
+   */
+  get assetsManager () { return this._assetsManager }
+  set assetsManager (val) { this._assetsManager = val }
+
+  constructor (canvas) {
+    this.canvas = canvas || document.getElementById('renderCanvas')
+    this.init()
+  }
+
+  /** initialize this class */
+  init () {
+    this.engine = new BABYLON.Engine(this.canvas, true, null, false)
+
+    // initialize scene.
+    this.scene = new BABYLON.Scene(this.engine)
+    this.scene.clearColor = new BABYLON.Color4(0, 0, 0, 1)
+
+    // initialize main render camera.
+    this.camera = new BABYLON.FreeCamera('Default-Camera', new BABYLON.Vector3(0, 0, 10), this.scene)
+    this.camera.setTarget(BABYLON.Vector3.Zero())
+    this.camera.attachControl(this.canvas, true)
+
+    // initialize default light and shadow generator.
+    this.light = new BABYLON.PointLight('Default-Light', new BABYLON.Vector3(0, 1, 0), this.scene)
+    this.light.intensity = 0.7
+    this.shadowGenerator = new BABYLON.ShadowGenerator(SHADOW_GENERATOR_SIZE, this.light)
+    this.shadowGenerator.useBlurExponentialShadowMap = true
+    this.shadowGenerator.useCloseExponentialShadowMap = true
+
+    // initialize pipeline
+    this.pipeline = new BABYLON.DefaultRenderingPipeline('Default-Pipeline', true, this.scene, [this.camera], true)
+
+    this.pipeline.samples = 4 // Multisample Anti-Aliasing, range 1-4, default 1
+    this.pipeline.fxaaEnabled = true // Fast Approximate Anti-Aliasing, default false
+    this.pipeline.imageProcessing.toneMappingEnabled = false // Tone Mapping, default false
+    this.pipeline.imageProcessing.contrast = 1 // Camera contrast, range 1-4, default 1
+    this.pipeline.imageProcessing.exposure = 1.25 // Camera exposure, range 1-4, default 1
+    this.pipeline.bloomEnabled = true // Bloom, default false
+    this.pipeline.bloomKernel = 84 // Kernel, range 1-500, default 64
+    this.pipeline.bloomWeight = 0.6 // Weight
+    this.pipeline.bloomThreshold = 0.15 // Threshold
+    this.pipeline.bloomScale = 0.45 // Scale
+
+    this.assetsManager = new BABYLON.AssetsManager(this.scene)
+
+    window.addEventListener('resize', () => {
+      this._engine.resize()
+    })
+
+    return this
+  }
+
+  start () {
+    this.engine.runRenderLoop(() => this.scene.render())
+    DEBUG && this.scene.debugLayer.show()
+
+    new Scenehouse(this).run()
+  }
+}
+
+window.addEventListener('DOMContentLoaded', () => new Game().start())
+
+export default Game
